184478092b6b61dc2f93bd2dbaa44aca4321423a
Chrystal Zou <chrystalzou@gmail.com>
2018-08-03 07:40:56 -0700

    server files

--- server/app.js
+++ server/app.js
@@ -1,5 +1,36 @@
 const app = require('express')()
 const firebase = require('./firebase')
+const bodyParser = require('body-parser')
+var cors = require('cors');
 const port = 5000
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = firebase.database().ref();
+
+// CREATE
+app.post(`/coins`, function (req, res) {
+  let coin = firebase.database().ref(`coins/${req.body.coin}`)
+  coin.set({
+    name: req.body.coin,
+  });
+  res.end();
+})
+
+// READ
+app.get('/coins', (req, res) => {
+  db.on("value", function(snapshot) {
+    res.json(snapshot.val());
+  }, function (error) {
+    console.log("Error: " + error.code);
+  });
+})
+
+// DELETE
+app.delete(`/coins/:id`, (req, res) => {
+  let coin = firebase.database().ref(`coins/${req.params.id}`);
+  coin.remove();
+  res.end();
+})
